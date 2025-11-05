98c9663f09010ca7ae94c7cfe57be7370c26d6bb
Dave <zenith@Daves-MacBook-Air.local>
2025-11-05 23:12:57 +0530

    /feat: Timer can start

--- app.js
+++ app.js
@@ -12,9 +12,27 @@ const appTimer=()=>{
         state=false;
         let totalSeconds=sessionAmount*60;
 
-        const updateSeconds=()=>{
-            //function code to be added.
-        };
+        const updateSeconds = () => {
+  const minuteDiv = document.querySelector(".minutes");
+  const secondDiv = document.querySelector(".seconds");
+
+  totalSeconds--;
+
+  let minutesLeft = Math.floor(totalSeconds / 60);
+  let secondsLeft = totalSeconds % 60;
+
+  if (secondsLeft < 10) {
+    secondDiv.textContent = "0" + secondsLeft;
+  } else {
+    secondDiv.textContent = secondsLeft;
+  }
+  minuteDiv.textContent = `${minutesLeft}`;
+
+  if (minutesLeft === 0 && secondsLeft === 0) {
+    bells.play();
+    clearInterval(myInterval);
+  }
+};
         myInterval=setInterval(updateSeconds,1000);
     }
     else{
@@ -22,3 +40,4 @@ const appTimer=()=>{
     }
     
 }
+startBtn.addEventListener("click", appTimer);
